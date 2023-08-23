5f0d2f93eb702063f77caf8a92feaafb64785a29
Akshay <akshaynaikanware19@gmail.com>
2023-08-23 22:00:10 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GalleryReact from './TabMenu/GalleryReact';
+
+function App() {
+  return (
+    <div className="App">
+     <GalleryReact/>
+    </div>
+  );
+}
+
+export default App;
